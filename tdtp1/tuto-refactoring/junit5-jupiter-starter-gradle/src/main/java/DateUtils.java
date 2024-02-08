42d910deeef1ebd00feb120e9d0a9be92e601d28
Quentin Robert <robertquentin29@gmail.com>
2024-02-08 10:49:40 +0100

    debut tp2

--- tdtp1/tuto-refactoring/junit5-jupiter-starter-gradle/src/main/java/DateUtils.java
+++ tdtp1/tuto-refactoring/junit5-jupiter-starter-gradle/src/main/java/DateUtils.java
@@ -2,16 +2,19 @@ import java.time.LocalDate;
 
 public class DateUtils {
     public static boolean isNowBetween(LocalDate startingDate, LocalDate endingDate) {
-        return isDateBetween(LocalDate.now(), startingDate, endingDate);
+        return isDateBetween(LocalDate.now(), startingDate, endingDate, true);
     }
 
-    static boolean isDateBetween(LocalDate date, LocalDate startingDate,
-                                 LocalDate endingDate) {
+    public static boolean isDateBetween(LocalDate date, LocalDate startingDate,
+                                        LocalDate endingDate, boolean inclusive) {
+        if (inclusive) {
+            return date.isBefore(endingDate.plusDays(1)) && date.isAfter(startingDate.minusDays(1));
+        }
         return date.isBefore(endingDate) && date.isAfter(startingDate);
     }
 
     public static boolean isDateOutside(LocalDate date, LocalDate startingDate,
                                        LocalDate endingDate) {
-        return !isDateBetween(date, startingDate, endingDate);
+        return !isDateBetween(date, startingDate, endingDate, true);
     }
 }
